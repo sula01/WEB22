9636d09e281ebe5e6b4d557ffff014980f74292d
sula01 <sultanbek.agabek@gmail.com>
2022-03-13 20:28:50 +0600

    lab5

---
+++ lab5/Updated-Online-store/src/app/categories.ts
@@ -0,0 +1,20 @@
+export const categories = [
+    {
+      name: 'Computers & Accessories',
+      img: 'https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_PC_2x._SY608_CB431800964_.jpg'
+    },
+    {
+      name: 'Get fit at home',
+      img: 'https://images-na.ssl-images-amazon.com/images/G/01/events/GFAH/GWDesktop_SingleImageCard_fitathome_2x._SY608_CB434924743_.jpg'
+    },
+    {
+      name: 'Beauty picks',
+      img: 'https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_Beauty_2x._SY608_CB432774344_.jpg'
+    },
+    {
+      name: 'Find your ideal TV',
+      img: 'https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_TV_2X._SY608_CB432517900_.jpg'
+    },
+  
+];
+
